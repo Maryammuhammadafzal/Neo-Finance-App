8ce29a718ebe15c01a738de96dfe7cec90e3d9ef
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-04-21 00:56:02 +0500

    change

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv"
 
 dotenv.config();
 
-const port = process.env.BACKEND_PORT || 5000;
+const port = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGODB_URL)
 .then(() => {
