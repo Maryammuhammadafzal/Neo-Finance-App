336d19a90a3a249d5f56e2fc70ad109dae8a6774
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-04-21 19:08:15 +0500

    change the server.js

--- server/server.js
+++ server/server.js
@@ -1,16 +1,44 @@
-import app from "./app.js";
-import mongoose from "mongoose";
+import express from "express"
 import dotenv from "dotenv"
+import mongoose from "mongoose";
+import cors from "cors"
 
 dotenv.config();
 
+const app = express();
+
 const port = process.env.PORT || 5000;
 
+// Cors Origins
+app.use(
+	cors({
+		origin: [
+			'http://localhost:5000/',
+			'http://localhost:3000',
+			'https://neo-finance-app-production.up.railway.app/',
+			'https://neo-finance-app.vercel.app/',
+		],
+		methods: ['GET', 'PUT', 'POST', 'DELETE'],
+		credentials: true,
+		allowedHeaders: ['Content-Type', 'Authorization'],
+	}),
+);
+app.use(express.json());
+
+// Mongodb Connection 
 mongoose.connect(process.env.MONGODB_URL)
 .then(() => {
         console.log("Mongodb connected");
-        app.listen(port , console.log(`App listen on ${port}`));
 })
 .catch((error) => {
         console.log(`Mongodb error ${error}`);
+})
+
+// Api 
+app.get("/" , (req , res) => {
+        res.send("Neo_Finance APi is Running");
+})
+
+app.listen(port , () => {
+	 console.log(`App listen on ${port}`);
 })
