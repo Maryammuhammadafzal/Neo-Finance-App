760cf58bde58164d54af6d7a274def7211e0bd53
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-04-21 13:16:30 +0500

    check the port

--- neo-finance-app/app/layout.js
+++ neo-finance-app/app/layout.js
@@ -1,7 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
+
 import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({
   subsets: ["latin"],
